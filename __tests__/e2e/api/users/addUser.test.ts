cd8a95cf2dc17e3f0d1f0335d24d8bcbad1d848f
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-01-24 19:08:08 +0300

    add CRD test for /users/

---
+++ __tests__/e2e/api/users/addUser.test.ts
@@ -0,0 +1,63 @@
+import {userRepository} from "../../../../src/repository/userMongoDbRepository";
+import request from "supertest";
+import {app} from "../../../../src";
+import {HTTP_STATUSES} from "../../../../src/routes/types/HttpStatuses";
+import {UserInputModel} from "../../../../src/routes/inputModels/UserInputModel";
+
+describe('/users', () => {
+    beforeAll(async () => {
+        await userRepository.deleteAllUsers();
+    });
+
+    it('check that only admin can add new user', async () => {
+        const inputUserModel: UserInputModel = {
+            "login": "us",
+            "password": "12345",
+            "email": "usergmailcom"
+        }
+
+        await request(app)
+            .post('/users')
+            .send(inputUserModel)
+            .expect(HTTP_STATUSES.UNAUTHORIZED_401)
+    })
+
+    it('validate input data for registration new user and return 400 with 3 errors', async () => {
+        const inputUserModel: UserInputModel = {
+            "login": "us",
+            "password": "12345",
+            "email": "usergmailcom"
+        }
+        const validationResult = await request(app)
+            .post('/users')
+            .auth('admin', 'qwerty', {type: "basic"})
+            .send(inputUserModel)
+            .expect(HTTP_STATUSES.BAD_REQUEST_400)
+
+        expect(validationResult.body).toEqual({
+            errorsMessages: expect.any(Array)
+        })
+
+        expect(validationResult.body.errorsMessages.length).toBe(3)
+    })
+
+    it('successfully add new user with valid input data and return ', async () => {
+        const inputUserModel: UserInputModel = {
+            "login": "user1",
+            "password": "123456",
+            "email": "user1-test@gmail.com"
+        }
+        const newUser = await request(app)
+            .post('/users')
+            .auth('admin', 'qwerty', {type: "basic"})
+            .send(inputUserModel)
+            .expect(HTTP_STATUSES.CREATED_201)
+
+        expect(newUser.body).toEqual({
+            "id": expect.any(String),
+            "login": "user1",
+            "email": "user1-test@gmail.com",
+            "createdAt": expect.any(String)
+        })
+    })
+});
---
+++ __tests__/e2e/api/users/deleteUser.test.ts
@@ -0,0 +1,45 @@
+import {userRepository} from "../../../../src/repository/userMongoDbRepository";
+import request from "supertest";
+import {app} from "../../../../src";
+import {HTTP_STATUSES} from "../../../../src/routes/types/HttpStatuses";
+import {UserInputModel} from "../../../../src/routes/inputModels/UserInputModel";
+
+let userIdForDelete: string;
+describe('/users', () => {
+    beforeAll(async () => {
+        await userRepository.deleteAllUsers();
+
+        const inputUserModel: UserInputModel = {
+            "login": "user1",
+            "password": "123456",
+            "email": "user1-test@gmail.com"
+        }
+        const newUser = await request(app)
+            .post('/users')
+            .auth('admin', 'qwerty', {type: "basic"})
+            .send(inputUserModel)
+            .expect(HTTP_STATUSES.CREATED_201)
+
+        userIdForDelete = newUser.body.id
+    });
+
+    it('check that only admin can delete a user', async () => {
+        await request(app)
+            .delete('/users/'+userIdForDelete)
+            .expect(HTTP_STATUSES.UNAUTHORIZED_401)
+    })
+
+    it('cant remove user that is not exists', async () => {
+        await request(app)
+            .delete('/users/63cff7135a31073c1f07e2e3')
+            .auth('admin', 'qwerty', {type: "basic"})
+            .expect(HTTP_STATUSES.NOT_FOUND_404)
+    })
+
+    it('delete user', async () => {
+        await request(app)
+            .delete('/users/'+userIdForDelete)
+            .auth('admin', 'qwerty', {type: "basic"})
+            .expect(HTTP_STATUSES.NO_CONTENT_204)
+    })
+});
