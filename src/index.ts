4e7156fd20a8fb77255499a6b163347ebc485aef
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-02 02:40:20 +0300

    remove confirm-registration end point

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,3 @@
-import {settings} from "./settings";
-import {RequestWithQuery} from "./routes/types/RequestTypes";
-import {RegistrationConfirmationCodeModel} from "./routes/types/RegistrationConfirmationCodeModel";
-import {app, startApp} from "./server";
-
-app.get('/confirm-registration', async (req: RequestWithQuery<RegistrationConfirmationCodeModel>, res) => {
-    const url = settings.APP_HOST + 'auth/registration-confirmation'
-    const result = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify({code: req.query.code})
-    })
-
-    res.sendStatus(result.status)
-})
+import {startApp} from "./server";
 
 startApp();
