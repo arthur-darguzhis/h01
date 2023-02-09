730951f9bdc624e968acab13aedb2e8c2a75b278
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-09 12:11:04 +0300

    add closure to rateLimiter => setRateLimiter(count, sec) that made it super flexible

--- src/common/middlewares/rateLimiterMiddleware.ts
+++ src/common/middlewares/rateLimiterMiddleware.ts
@@ -1,52 +1,63 @@
 import {NextFunction, Request, Response} from "express";
 import {TooManyRequest} from "../exceptions/TooManyRequest";
 
-class RateLimiter {
+type rateLimiterContainer = {
+    [key: string]: RateLimiter
+}
 
-    private static singletone: RateLimiter;
+class RateLimiter {
+    private static container: rateLimiterContainer = {};
     countOfRequestsFromIP;
     timePeriodInSec;
-    map: any;
-
-    public static manager() {
-        if (RateLimiter.singletone) {
-            return RateLimiter.singletone
-        }
-        RateLimiter.singletone = new RateLimiter(5, 10)
-        return RateLimiter.singletone
-    }
+    map: Array<any> = [];
 
     private constructor(countOfRequestsFromIp: number, timePeriodInSec: number) {
         this.countOfRequestsFromIP = countOfRequestsFromIp
         this.timePeriodInSec = timePeriodInSec
-        this.map = new Map<string, number>()
+    }
+
+    public static manager(count = 5, sec = 10) {
+        const key = `${count}_${sec}`;
+        if (RateLimiter.container && RateLimiter.container.hasOwnProperty(key)) {
+            return RateLimiter.container[key]
+        }
+        RateLimiter.container[key] = new RateLimiter(count, sec)
+        return RateLimiter.container[key]
     }
 
     putData(ip: any, route: any): void | never {
         if (!this.map.hasOwnProperty(ip)) {
-            this.map[ip] = {}
+            this.map[ip] = []
         }
-        const ipCollection = this.map[ip];
 
         if (!this.map[ip].hasOwnProperty(route)) {
             this.map[ip][route] = []
         }
 
-        const routeData: Array<number> = this.map[ip][route]
+        const lastRequestsData: Array<number> = this.map[ip][route]
 
-        this.map[ip][route] = routeData.filter((value) => {
+        const lastRequestsInTimeLimit = lastRequestsData.filter((value) => {
             return (new Date().getTime() - value) < (10 * 1000)
         })
 
-        if((this.map[ip][route].length === 5)){
+        if ((lastRequestsInTimeLimit.length === 5)) {
             throw new TooManyRequest('Too many request')
         }
+
         this.map[ip][route].push(new Date().getTime());
-        }
+    }
 }
 
 export const checkRateLimiterMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const rateLimiter = RateLimiter.manager()
     rateLimiter.putData(req.ip, req.originalUrl);
-next();
+    next();
+}
+
+export const setRateLimiter = (cout: number, sec: number) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const rateLimiter = RateLimiter.manager(cout, sec)
+        rateLimiter.putData(req.ip, req.originalUrl);
+        next();
+    }
 }
