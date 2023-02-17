1a35601fac884d5121e02758118952438dc5bc1d
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-17 18:32:18 +0300

    refactoring repositories

--- src/modules/auth/passwordRecovery/passwordRecoveryRepository.ts
+++ src/modules/auth/passwordRecovery/passwordRecoveryRepository.ts
@@ -1,14 +1,43 @@
-import {CommandMongoDbRepository} from "../../../common/repositories/CommandMongoDbRepository";
 import {PasswordRecoveryType} from "./types/PasswordRecoveryType";
 import {EntityNotFound} from "../../../common/exceptions/EntityNotFound";
 import {PasswordRecoveryModel} from "./model/PasswordRecoveryModel";
 
-class PasswordRecoveryRepository extends CommandMongoDbRepository<PasswordRecoveryType, object> {
+class PasswordRecoveryRepository {
+    async add(passwordRecovery: PasswordRecoveryType): Promise<PasswordRecoveryType> {
+        await PasswordRecoveryModel.create(passwordRecovery)
+        return passwordRecovery
+    }
+
+    async isExists(id: string): Promise<boolean> {
+        const entity = await PasswordRecoveryModel.findOne({_id: id})
+        return !!entity;
+    }
+
+    async find(id: string): Promise<PasswordRecoveryType | null> {
+        return PasswordRecoveryModel.findOne({_id: id});
+    }
+
+    async get(id: string): Promise<PasswordRecoveryType | never> {
+        const entity = await PasswordRecoveryModel.findOne({_id: id});
+        if (!entity) throw new EntityNotFound(`Password Recovery Code with ID: ${id} is not exists`);
+        return entity
+    }
+
+    async update(id: string, updateFilter: object): Promise<boolean> {
+        const result = await PasswordRecoveryModel.updateOne({_id: id}, {$set: updateFilter})
+        return result.modifiedCount === 1;
+    }
+
+    async delete(id: string): Promise<boolean> {
+        const result = await PasswordRecoveryModel.deleteOne({_id: id});
+        return result.deletedCount === 1;
+    }
+
     async getByCode(code: string): Promise<PasswordRecoveryType | never> {
-        const passwordRecovery = await this.model.findOne({code: code})
+        const passwordRecovery = await PasswordRecoveryModel.findOne({code: code})
         if (!passwordRecovery) throw new EntityNotFound("it's not a password recovery");
         return passwordRecovery;
     }
 }
 
-export const passwordRecoveryRepository = new PasswordRecoveryRepository(PasswordRecoveryModel, 'Password Recovery Code');
+export const passwordRecoveryRepository = new PasswordRecoveryRepository();
--- src/modules/blog/repository/blog.MongoDbRepository.ts
+++ src/modules/blog/repository/blog.MongoDbRepository.ts
@@ -1,11 +1,38 @@
 import {BlogInputModel} from "../types/BlogInputModel";
 import {BlogType} from "../types/BlogType";
-import {CommandMongoDbRepository} from "../../../common/repositories/CommandMongoDbRepository";
 import {BlogModel} from "../model/BlogModel";
+import {EntityNotFound} from "../../../common/exceptions/EntityNotFound";
 
-export class BlogRepository extends CommandMongoDbRepository<BlogType, BlogInputModel> {
+export class BlogRepository {
+    async add(newEntity: BlogType): Promise<BlogType> {
+        await BlogModel.create(newEntity)
+        return newEntity
+    }
 
+    async isExists(id: string): Promise<boolean> {
+        const entity = await BlogModel.findOne({_id: id})
+        return !!entity;
+    }
+
+    async find(id: string): Promise<BlogType | null> {
+        return BlogModel.findOne({_id: id});
+    }
+
+    async get(id: string): Promise<BlogType | never> {
+        const entity = await BlogModel.findOne({_id: id});
+        if (!entity) throw new EntityNotFound(`Blog with ID: ${id} is not exists`);
+        return entity
+    }
+
+    async update(id: string, updateFilter: BlogInputModel): Promise<boolean> {
+        const result = await BlogModel.updateOne({_id: id}, {$set: updateFilter})
+        return result.modifiedCount === 1;
+    }
+
+    async delete(id: string): Promise<boolean> {
+        const result = await BlogModel.deleteOne({_id: id});
+        return result.deletedCount === 1;
+    }
 }
 
-export const blogRepository = new BlogRepository(BlogModel, 'Blog')
-//asdf
+export const blogRepository = new BlogRepository()
