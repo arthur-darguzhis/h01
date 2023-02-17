ab836a7db03f71b1d3f055e8f97dc1145c719abd
arthur.darguzhis <arthur.darguzhis@gmail.com>
2023-02-17 22:25:20 +0300

    refactoring routers to controllers Classes

--- src/modules/user/types/UserType.ts
+++ src/modules/user/types/UserType.ts
@@ -14,12 +14,3 @@ export class User {
         this.createdAt = new Date().toISOString()
     }
 }
-
-// export type UserType = {
-//     _id: string
-//     login: string,
-//     password: string,
-//     email: string,
-//     createdAt: string
-//     isActive: boolean
-// }
